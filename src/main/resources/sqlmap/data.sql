8ab305844a970d30546728cbae009601ef69bff9
dhSeong <sp10773p@gmail.com>
2016-11-20 17:27:18 +0900

    -

--- src/main/resources/sqlmap/data.sql
+++ src/main/resources/sqlmap/data.sql
@@ -1,24 +1,3 @@
-insert into users values('rednics', 'Shin Kwan Young', '1234');
-
-insert into cm_code values('CODE1', 'Banana', 'Banana');
-insert into cm_code values('CODE2', 'Apple', 'Apple');
-
-insert into TB_TEST values(1, '20160910', 'Client1', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(2, '20160911', 'Client2', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(3, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(4, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(5, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(6, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(7, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(8, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(9, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(10, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(11, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-insert into TB_TEST values(12, '20160912', 'Client3', 100000, 10000, 110000, 'tax');
-
-insert into TB_DETAIL_TEST values(1, 'COL1', 'COL2', '20160801', 'A', 'M, S', 'M', 'COL7', 'COL8', '20161001', '20161003', 'COL11', 'COL12', 'COL13', 'COL14', 'COL15', 'COL16', 17, 18, 19, 'COL20');
-insert into TB_DETAIL_TEST values(2, '2COL1', '2COL2', '20160903', 'B', 'S', 'S', 'COL7', 'COL8', '20161004', '20161006', 'COL11', 'COL12', 'COL13', 'COL14', 'COL15', 'COL16', 17, 18, 19, 'COL20');
-
 INSERT INTO CM_MENU VALUES('CM'         , 'Setting' , 1, 'A', ''     , 1, NULL        , 'Y', 'N');
 INSERT INTO CM_MENU VALUES('CM_SYST'    , 'Admin'   , 2, 'A', 'CM'      , 1, NULL        , 'Y', 'N');
 INSERT INTO CM_MENU VALUES('CM_SYST_101', 'Menu', 3, 'A', 'CM_SYST' , 1, 'mainView.do'    , 'Y', 'Y');
